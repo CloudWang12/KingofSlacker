62aaba4939ae5aeeb756e8917b52ec419a216ade
CloudWang <cloudwang12@outlook.com>
2025-11-20 18:39:46 +0800

    Initial commit

---
+++ KingofSlacker/Source/KingofSlacker/KingofSlacker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KingofSlacker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KingofSlacker, "KingofSlacker" );
